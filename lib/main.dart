ee0b09af510a000d7e5d1ed6fb03e33a501485db
David Wong <david20030701@gmail.com>
2024-03-16 22:48:54 +0800

    initialize

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,23 @@
   One tab contains a button with text "Add" on it.
   Another tab shows a number.
 */
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ;
+  }
+}
