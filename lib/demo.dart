e9c5785345811c508c9430201fba9d4654eaca89
David Wong <david20030701@gmail.com>
2024-03-19 02:36:08 +0800

    small change

--- lib/demo.dart
+++ lib/demo.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 19, 191, 128)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 19, 191, 128)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
